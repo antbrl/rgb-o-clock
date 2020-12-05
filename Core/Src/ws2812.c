a44dfdaecd07f1f1a901131ae8b1e7338d9e2fa0
Antoine Brûlé <antoine.brule@netc.eu>
2020-12-05 12:33:17 +0100

    add getLEDcolor and mixLEDcolor functions

--- Core/Src/ws2812.c
+++ Core/Src/ws2812.c
@@ -33,6 +33,20 @@ void saveLEDbuffer(void)
 	memcpy(LEDbuffer, LEDbuffer_temp, LED_BUFFER_SIZE);
 }
 
+void getLEDcolor(uint32_t LEDnumber, uint8_t *RED, uint8_t *GREEN, uint8_t *BLUE)
+{
+	uint32_t i;
+	uint32_t LEDindex = LEDnumber % LED_NUMBER;
+	*RED = *GREEN = *BLUE = 0;
+
+	for (i = 0; i < 8; i++) // GREEN data
+		*GREEN |= ((LEDbuffer[RESET_SLOTS_BEGIN + LEDindex * 24 + i]) >= WS2812_1) << i;
+	for (i = 0; i < 8; i++) // RED
+		*RED |= ((LEDbuffer[RESET_SLOTS_BEGIN + LEDindex * 24 + i + 8]) >= WS2812_1) << i;
+	for (i = 0; i < 8; i++) // BLUE
+		*BLUE |= ((LEDbuffer[RESET_SLOTS_BEGIN + LEDindex * 24 + i + 16]) >= WS2812_1) << i;
+}
+
 void setLEDcolor(uint32_t LEDnumber, uint8_t RED, uint8_t GREEN, uint8_t BLUE)
 {
 	uint8_t tempBuffer[24];
@@ -49,6 +63,13 @@ void setLEDcolor(uint32_t LEDnumber, uint8_t RED, uint8_t GREEN, uint8_t BLUE)
 
 	for (i = 0; i < 24; i++)
 		LEDbuffer[RESET_SLOTS_BEGIN + LEDindex * 24 + i] = tempBuffer[i];
+}
+
+void mixLEDcolor(uint32_t LEDnumber, uint8_t RED, uint8_t GREEN, uint8_t BLUE)
+{
+	uint8_t r, g, b;
+	getLEDcolor(LEDnumber, &r, &g, &b);
+	setLEDcolor(LEDnumber, (r + RED) % 256, (g + GREEN) % 256, (b + BLUE) % 256);
 }
 
 void setWHOLEcolor(uint8_t RED, uint8_t GREEN, uint8_t BLUE)
